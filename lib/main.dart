e15b58203eb12b4ec82afcae66f7da41d1b3499e
Hashim Omar <hashimomar6@gmail.com>
2024-09-25 21:48:30 -0400

    Initial project setup and structure

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherInfoApp());
+}
+
+class WeatherInfoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather Info App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather Info'),
+      ),
+      body: Center(
+        child: Text('Welcome to Weather Info App!'),
+      ),
+    );
+  }
+}
